af595680d6e9aeb44c1792e83d25118ebe781177
mellowdeep <mellowdeep@abv.bg>
2018-04-07 10:31:26 +0300

    added shop page

--- js/application.js
+++ js/application.js
@@ -1,18 +1,18 @@
 class Application {
-  constructor({ view, router }) {
-    this.router = router;
-    router.routerMap.forEach(route => {
-      const { model, controller, pathname } = route;
-      route.render = () =>
-        view.createRenderFunction({ model, controller })(pathname);
-      controller.methods._route = router.startRouting(view);
-    });
-  }
+    constructor({view, router}) {
+        this.router = router;
+        router.routerMap.forEach(route => {
+            const {model, controller, pathname} = route;
+            route.render = () =>
+                view.createRenderFunction({model, controller})(pathname);
+            controller.methods._route = router.startRouting(view);
+        });
+    }
 }
 
 const mainDataInput = {
-  login: 'userlogin',
-  userName: 'Andrei',
+    login: 'userlogin',
+    userName: 'Andrei',
 };
 
 // const data = {
@@ -24,7 +24,8 @@ const mainDataInput = {
 // };
 
 const methods = {
-  showClick(d) {},
+    showClick(d) {
+    },
 };
 
 const storage = new Storage(mainDataInput);
@@ -33,12 +34,12 @@ const view = new View();
 const model = new Model({}, storage);
 const controller = new Controller(methods);
 const router = new Router([
-  { pathname: 'index_fw2.html', model: model, controller: controller },
-  { pathname: 'test.html', model: model, controller: controller },
-  { pathname: 'index.html', model: model, controller: controller },
-  { pathname: 'review.html', model: model, controller: controller },
-  { pathname: 'news.html', model: model, controller: controller }
+    {pathname: 'index_fw2.html', model: model, controller: controller},
+    {pathname: 'test.html', model: model, controller: controller},
+    {pathname: 'index.html', model: model, controller: controller},
+    {pathname: 'review.html', model: model, controller: controller},
+    {pathname: 'news.html', model: model, controller: controller}
 ]);
 
-const app = new Application({ view, model, controller, router });
+const app = new Application({view, model, controller, router});
 app.router.routerMap.find((route) => route.pathname === location.pathname.slice(location.pathname.lastIndexOf("/") + 1)).render();
